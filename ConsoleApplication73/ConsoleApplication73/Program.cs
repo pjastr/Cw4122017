12fd3a08dbd70c9e3815d0027eaf89462159753e
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-04 12:50:25 +0100

    Add files via upload

---
+++ ConsoleApplication73/ConsoleApplication73/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication73
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unchecked
+            {
+                int a = int.MaxValue;
+                Console.WriteLine(a);
+                try
+                {
+                    Console.WriteLine(++a);
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
