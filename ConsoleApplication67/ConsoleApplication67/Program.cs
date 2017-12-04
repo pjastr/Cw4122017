82ca394c03f10b03b1370a77382a454edc5c0310
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-04 12:48:26 +0100

    Add files via upload

---
+++ ConsoleApplication67/ConsoleApplication67/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba1 = 30;
+            int liczba2 = 0;
+            try
+            {
+                int liczba3 = liczba1 / liczba2;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Data);
+            }
+            finally
+            {
+                Console.WriteLine("FINALLY");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication68/ConsoleApplication68/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication68
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("podaj liczbe");
+            int a=-1;
+            while(true)
+            {
+                try
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                    if (a < 0) throw new ArgumentOutOfRangeException();
+                    break;
+                    
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Błędny format, podaj liczbę dodatnią");
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine("liczba ujemna, podaj liczbę dodatnią");
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine("liczba spoza zakresu zmiennej, podaj liczbę dodatnią");
+                }   
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
+            Console.WriteLine("Pierwiastek= {0}", Math.Sqrt(a));
+            Console.ReadLine();
+        }
+    }
+}
