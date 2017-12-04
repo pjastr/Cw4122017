65452ce34788e0b6f5f88a9f4980c7ec429c66ca
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-04 12:49:48 +0100

    Add files via upload

---
+++ ConsoleApplication70/ConsoleApplication70/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] table = new int[2];
+            try
+            {
+                table[1] = 2;
+                throw new ArgumentNullException();
+            }
+            
+            catch(IndexOutOfRangeException e)
+            {
+                Console.WriteLine("poza zakresem");
+            }
+            catch
+            {
+                Console.WriteLine("inny wyjątek");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication71/ConsoleApplication71/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ConsoleApplication71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new NaszWyjatekException("sdddffdf");
+        }
+    }
+
+    class NaszWyjatekException:System.Exception
+    {
+        public NaszWyjatekException() { }
+
+        public NaszWyjatekException(string message) : base(message) { }
+        public NaszWyjatekException(String message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
